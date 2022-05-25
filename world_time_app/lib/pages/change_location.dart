bac4da9539a437b3c6a499ffb04c57bd1590e9a1
Surajit <piklu3043@gmail.com>
2022-05-25 21:45:34 +0530

    03: Making change location screen

--- world_time_app/lib/pages/change_location.dart
+++ world_time_app/lib/pages/change_location.dart
@@ -1,4 +1,4 @@
-
+import 'package:world_time_app/services/world_time.dart';
 import 'package:flutter/material.dart';
 
 class Location extends StatefulWidget {
@@ -10,15 +10,41 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
 
+  List<WorldTime> locations = [
+    WorldTime(url: 'Asia/Kolkata', location: 'Kolkata', flag: 'india.png'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // In appBar it automatically puts a back-arrow(<--) in it, if we come from a different screen
       appBar: AppBar(
-        backgroundColor: Colors.grey[600],
+        backgroundColor: Colors.blue[600],
         title: const Text('Choose a Location'),
         // Using `elevation` to cancel the drop shadow, and make the appBar 'flat' on the screen
         elevation: 0.0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              ),
+            ),
+          );
+        },
       ),
       
     );
--- world_time_app/lib/pages/loading.dart
+++ world_time_app/lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:world_time_app/services/world_time';
+import 'package:world_time_app/services/world_time.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
