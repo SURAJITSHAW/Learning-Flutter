9f896ee202284776394c2a0aae6c5b3fdfe9b70b
Surajit <piklu3043@gmail.com>
2022-05-25 12:22:37 +0530

    03: create a world_time separate file, where we put our logic to extract data in a class. Also render real time of berlin using api in our UI

--- world_time_app/lib/pages/loading.dart
+++ world_time_app/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'; // Import http pacakage before using it.
-import 'dart:convert';
+import 'package:world_time_app/services/world_time';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,41 +10,23 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
+  String time = 'Loading...';
 
-    // get the data from the api endpoint, await unit we get the data and store it into a response object of type Response.
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
-
-    // on that response object had a body prop, which is the actual json string we get from the api call, the decode that data to Map type to store in data variable to use it.
-    Map data = jsonDecode(response.body);
-    
-
-    // Get props from the data
-    String dateTime = data['utc_datetime'];
-    String offset = data['utc_offset'];
-    String offset_hours = offset.substring(1, 3);
-    String offset_minutes = offset.substring(4);
-
-    var nowTime = data['datetime'];
-    print(nowTime.runtimeType);
-
-    print(offset.substring(1, 3));
-    print(offset.substring(4));
-    // print(data['datetime']); Using datetime prop we don't had to do this crap of adding offset and all that.
-    
-    // Create a DateTime object
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offset_hours)));
-    now = now.add(Duration(minutes: int.parse(offset_minutes)));
-    print(now);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(flag: 'germany.png', location: 'Berlin', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
+
 
   @override
   void initState() {
     super.initState();
 
-    getTime();
+    setupWorldTime();
   }
 
   @override
@@ -53,7 +34,10 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey,
-        title: const Text('Loading...'),
+        title: const Text('Loading Screen'),
+      ),
+      body: Center(
+        child: Text(time),
       ),
     );
   }
