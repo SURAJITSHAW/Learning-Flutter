826ce468144b7644d37b8aca64a639fc59519102
Surajit <piklu3043@gmail.com>
2022-05-25 15:09:02 +0530

    03: Adding location name, live time of that place in home screen

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -14,21 +16,44 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = ModalRoute.of(context)!.settings.arguments as Map; // Getting the data passed by the loading route
     print(data);
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
         children: [
-          TextButton.icon(
-              onPressed: (() {
-                Navigator.pushNamed(context, '/location');
-              }),
-              icon: const Icon(Icons.edit_location),
-              label: const Text('Edit Location'))
+            TextButton.icon(
+                onPressed: (() {
+                  Navigator.pushNamed(context, '/location');
+                }),
+                icon: const Icon(Icons.edit_location),
+                label: const Text('Edit Location')),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.0
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              data['time'],
+              style: const TextStyle(
+                fontSize: 66.0,
+              ),
+            )
         ],
-      )),
+      ),
+          )),
     );
   }
 }
