571e2900e32dd65afc0328269ad7493e5240d7cb
Surajit <piklu3043@gmail.com>
2022-05-25 20:47:51 +0530

    03: Setting up diff bg image for day and night

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -20,42 +20,52 @@ class _HomeState extends State<Home> {
     print(data);
 
     // Set background image
-    String bg_img = dat['isDayTime'] ? 'day.jpg' : 'night.jpg';
+    String bgImg = data['isDayTime'] ? 'day.jpg' : 'night.jpg';
+    Color? bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo[700];
 
     return Scaffold(
+        backgroundColor: bgColor,
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-            child: Column(
-        children: [
-            TextButton.icon(
-                onPressed: (() {
-                  Navigator.pushNamed(context, '/location');
-                }),
-                icon: const Icon(Icons.edit_location),
-                label: const Text('Edit Location')),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  data['location'],
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                    letterSpacing: 2.0
-                  ),
-                )
-              ],
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/$bgImg'), fit: BoxFit.cover,),
+              
             ),
-            const SizedBox(height: 20.0),
-            Text(
-              data['time'],
-              style: const TextStyle(
-                fontSize: 66.0,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 90.0, 0, 0),
+              child: Column(
+        children: [
+              TextButton.icon(
+                  onPressed: (() {
+                    Navigator.pushNamed(context, '/location');
+                  }),
+                  icon: Icon(Icons.edit_location, color: Colors.grey[300],),
+                  label: Text('Edit Location',  style: TextStyle(color: Colors.grey[300]),)),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 2.0,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
               ),
-            )
+              const SizedBox(height: 20.0),
+              Text(
+                data['time'],
+                style: const TextStyle(
+                  fontSize: 66.0,
+                  color: Colors.white,
+                ),
+              )
         ],
       ),
+            ),
           )),
     );
   }
